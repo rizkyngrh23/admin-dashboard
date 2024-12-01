752080b5a8949ed9be282b0f8ecb351c3bfee451
Rizky Nugraha <rizkynugraha999@gmail.com>
2024-12-01 21:01:39 +0700

    Update Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -179,15 +179,12 @@ export default {
           } catch (error) {
             console.error('Profile picture upload failed:', error);
             if (error.response) {
-              // Server responded with a status other than 200 range
               console.error('Response data:', error.response.data);
               console.error('Response status:', error.response.status);
               console.error('Response headers:', error.response.headers);
             } else if (error.request) {
-              // Request was made but no response received
               console.error('Request data:', error.request);
             } else {
-              // Something happened in setting up the request
               console.error('Error message:', error.message);
             }
             this.user.profilePicture = URL.createObjectURL(this.profilePictureFile);
